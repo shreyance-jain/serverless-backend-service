5c0233e076a6e8b897fecc43a4f956042f5497aa
shreyance-jain <shreyancejain94@gmail.com>
2022-05-28 18:23:53 +0530

    hello world lambda

---
+++ src/deliveries/lambda/hello-world.js
@@ -0,0 +1,22 @@
+const httpResponse = (statusCode, body, headers = {}) => ({
+  statusCode,
+  body: JSON.stringify(body),
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    ...headers,
+  },
+})
+
+const helloWorld = async (event) => {
+  try {
+    console.log('hello world from backend serverless service');
+    return httpResponse(200, {message: 'hello world serverless! Youre awesome'});
+  } catch (error) {
+    return httpResponse(error.statusCode || 500, { message: error.toString() || 'Something went wrong!' });
+  }
+}
+
+module.exports = {
+  handler: helloWorld,
+};
