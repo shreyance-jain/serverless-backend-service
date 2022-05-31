f4c6786c6101e4f5d9203a0fe4db4305b6e53b60
shreyance-jain <shreyancejain94@gmail.com>
2022-06-01 02:31:18 +0530

    hello world lambda lint fix

--- src/deliveries/lambda/hello-world.js
+++ src/deliveries/lambda/hello-world.js
@@ -6,16 +6,17 @@ const httpResponse = (statusCode, body, headers = {}) => ({
     'Access-Control-Allow-Origin': '*',
     ...headers,
   },
-})
+});
 
-const helloWorld = async (event) => {
+const helloWorld = async () => {
   try {
+    // eslint-disable-next-line no-console
     console.log('hello world from backend serverless service');
-    return httpResponse(200, {message: 'hello world serverless! Youre awesome'});
+    return httpResponse(200, { message: 'hello world serverless! Youre awesome' });
   } catch (error) {
     return httpResponse(error.statusCode || 500, { message: error.toString() || 'Something went wrong!' });
   }
-}
+};
 
 module.exports = {
   handler: helloWorld,
