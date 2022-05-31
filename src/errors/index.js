d481353f2ffe9189e561a32ec6bee70b5f6163cc
shreyance-jain <shreyancejain94@gmail.com>
2022-06-01 02:33:46 +0530

    custom errors

---
+++ src/errors/index.js
@@ -0,0 +1,11 @@
+class CustomError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.type = this.constructor.name;
+  }
+}
+
+module.exports = {
+  CustomError,
+};
