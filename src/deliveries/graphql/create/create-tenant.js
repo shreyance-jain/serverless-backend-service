550c559b0ad031bfc9c9658632db566c939b4b79
shreyance-jain <shreyancejain94@gmail.com>
2022-06-08 12:52:00 +0530

    tenant schema chnages, request response template changes to populate errors, create-tenant lamdba response changes

--- src/deliveries/graphql/create/create-tenant.js
+++ src/deliveries/graphql/create/create-tenant.js
@@ -2,7 +2,6 @@ const { createTenant } = require('../../../usecases');
 
 const { tenantAdaptors } = require('../../../adaptors');
 
-const { httpResponse } = require('../../../services/http');
 const { Logger } = require('../../../services/logger');
 
 const handler = async (event) => {
@@ -13,10 +12,10 @@ const handler = async (event) => {
     tenantAdaptors.createTenantAdaptor(params);
     const tenant = await createTenant(params);
     // response adaptor
-    return httpResponse(200, { tenant });
+    return tenant;
   } catch (error) {
     logger.debug(error.toString(), error, { tenant: params.tenant });
-    return httpResponse(error.statusCode || 500, { message: error.message || 'Failed to create tenant' });
+    throw new Error(error.message || 'Failed to create tenant', error);
   }
 };
 
