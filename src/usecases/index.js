705826c4bb629fd2eb9ecd753e198cccf6b4d142
shreyance-jain <shreyancejain94@gmail.com>
2022-06-09 00:30:41 +0530

    update lambda: usecase

--- src/usecases/index.js
+++ src/usecases/index.js
@@ -1,9 +1,11 @@
 const { createTenant } = require('./create-tenant');
 const { getTenant } = require('./get-tenant');
 const { deleteTenant } = require('./delete-tenant');
+const { updateTenant } = require('./update-tenant');
 
 module.exports = {
   createTenant,
   getTenant,
   deleteTenant,
+  updateTenant,
 };
