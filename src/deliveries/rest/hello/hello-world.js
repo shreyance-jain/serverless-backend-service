5b263138166cdf03ec931a41bc9e291a4cb269a0
shreyance-jain <shreyancejain94@gmail.com>
2022-06-11 13:35:24 +0530

    folder name changes

--- src/deliveries/rest/hello/hello-world.js
+++ src/deliveries/rest/hello/hello-world.js
@@ -10,8 +10,6 @@ const httpResponse = (statusCode, body, headers = {}) => ({
 
 const helloWorld = async () => {
   try {
-    // eslint-disable-next-line no-console
-    console.log('hello world from backend serverless service');
     return httpResponse(200, { message: 'hello world serverless! Youre awesome' });
   } catch (error) {
     return httpResponse(error.statusCode || 500, { message: error.toString() || 'Something went wrong!' });
